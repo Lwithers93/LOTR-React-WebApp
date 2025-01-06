76e985b1b0ab0ab820d6567c1e15ae3a82eee9f1
Luke <lwithers93@live.co.uk>
2025-01-06 13:06:05 +0000

    Updated package Scripts for build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import lotrLogo from "/icons8-one-ring-32.png";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 
