b8826252c831e21c2478792d4e889f69c2669273
Luke <lwithers93@live.co.uk>
2025-01-06 14:07:26 +0000

    Update base and homepage

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/LOTR-React-WebApp",
+  base: "/lotr-react-web-app",
   // define: {
   //   "process.env": {},
   // },
